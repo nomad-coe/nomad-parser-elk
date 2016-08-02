d422d711a6cf9d58fc339ca49c250fd8282f8e49
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-08-02 15:08:20 +0200

    first skeleton for the elk parser

---
+++ src/main/scala/eu/nomad_lab/parsers/ElkParser.scala
@@ -0,0 +1,42 @@
+package eu.nomad_lab.parsers
+
+import eu.nomad_lab
+import eu.nomad_lab.DefaultPythonInterpreter
+import org.{ json4s => jn }
+import eu.{ nomad_lab => lab }
+import scala.collection.breakOut
+
+object ElkParser extends SimpleExternalParserGenerator(
+  name = "ElkParser",
+  parserInfo = jn.JObject(
+    ("name" -> jn.JString("Elk")) ::
+      ("parserId" -> jn.JString("Elk" + lab.ElkVersionInfo.version)) ::
+      ("versionInfo" -> jn.JObject(
+        ("nomadCoreVersion" -> jn.JObject(lab.NomadCoreVersionInfo.toMap.map {
+          case (k, v) => k -> jn.JString(v.toString)
+        }(breakOut): List[(String, jn.JString)])) ::
+          (lab.ElkVersionInfo.toMap.map {
+            case (key, value) =>
+              (key -> jn.JString(value.toString))
+          }(breakOut): List[(String, jn.JString)])
+      )) :: Nil
+  ),
+  mainFileTypes = Seq("text/.*"),
+  mainFileRe = """:LABEL[0-9]+: using WIEN2k_(?<version>[0-9.]+) \(Release (?<release>[0-9/.]+)\) in """.r,
+  cmd = Seq(DefaultPythonInterpreter.pythonExe(), "${envDir}/parsers/elk/parser/parser-elk/parser_elk.py",
+    "--uri", "${mainFileUri}", "${mainFilePath}"),
+  resList = Seq(
+    "parser-elk/parser_elk.py",
+    "parser-elk/setup_paths.py",
+    "nomad_meta_info/public.nomadmetainfo.json",
+    "nomad_meta_info/common.nomadmetainfo.json",
+    "nomad_meta_info/meta_types.nomadmetainfo.json",
+    "nomad_meta_info/elk.nomadmetainfo.json"
+  ) ++ DefaultPythonInterpreter.commonFiles(),
+  dirMap = Map(
+    "parser-elk" -> "parsers/elk/parser/parser-elk",
+    "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
+    "python" -> "python-common/common/python/nomadcore"
+  ) ++ DefaultPythonInterpreter.commonDirMapping(),
+  metaInfoEnv = Some(lab.meta.KnownMetaInfoEnvs.elk)
+)
---
+++ src/test/scala/eu/nomad_lab/parsers/ElkParserSpec.scala
@@ -0,0 +1,14 @@
+package eu.nomad_lab.parsers
+
+import org.specs2.mutable.Specification
+
+object ElkTests extends Specification {
+  "ElkParserTest" >> {
+    "test with json-events" >> {
+      ParserRun.parse(Wien2kParser, "parsers/elk/test/examples/ok/ok.scf", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test with json" >> {
+      ParserRun.parse(Wien2kParser, "parsers/elk/test/examples/ok/ok.scf", "json") must_== ParseResult.ParseSuccess
+    }
+  }
+}
