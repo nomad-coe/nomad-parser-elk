e31d027301bc1333acd1abcd5ff4b16c255aad25
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:12:37 +0200

    Adapted further metainfo changes

--- elkparser/metainfo/elk.py
+++ elkparser/metainfo/elk.py
@@ -25,6 +25,9 @@ from nomad.metainfo import (  # pylint: disable=unused-import
 from nomad.datamodel.metainfo import run
 
 
+m_package = Package()
+
+
 class x_elk_section_lattice_vectors(MSection):
     '''
     lattice vectors
